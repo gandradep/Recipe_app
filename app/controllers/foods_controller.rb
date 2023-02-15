a08ecb5ea4dbdeb75887fbe218bc582cd9b88fce
Josué Benavides <jdbs9514@gmail.com>
2023-02-15 16:48:59 -0500

    added the food list and create method to add new food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -13,11 +13,14 @@ class FoodsController < ApplicationController
 
   # POST /foods or /foods.json
   def create
+    @user = User.first
     @food = Food.new(food_params)
+    @food.user = @user
+
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -45,6 +48,6 @@ class FoodsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def food_params
-    params.fetch(:food, {})
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
   end
 end
