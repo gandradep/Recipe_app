5e5bf189784d7b48e7e76e1884b5bdb3d66a190c
Josué Benavides <jdbs9514@gmail.com>
2023-02-15 22:54:08 -0500

    added devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true, allow_blank: false
   has_many :foods
   has_many :recipes
