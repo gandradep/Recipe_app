624a4119bb3f4fcd788c3299d85831feab603c02
Josué Benavides <jdbs9514@gmail.com>
2023-02-16 19:28:46 -0500

    skip the authentication to display the public recipes page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,11 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: :public_recipes
 
   # GET /recipes or /recipes.json
   def index
     # logged in user recipes
-    @user_test = User.first
+    @user_test = current_user
     @recipes = Recipe.where(user: @user_test)
   end
 
@@ -23,7 +24,7 @@ class RecipesController < ApplicationController
 
   # POST /recipes or /recipes.json
   def create
-    usert = User.first
+    usert = current_user
     @recipe = Recipe.new(recipe_params)
     @recipe.user = usert
     respond_to do |format|
