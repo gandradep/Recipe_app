f569af180175f364c689157b2165ea5849fec79e
Josué Benavides <jdbs9514@gmail.com>
2023-02-16 19:32:24 -0500

    fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,11 @@ class ApplicationController < ActionController::Base
   def storable_location?
     request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
   end
-  
+
   def store_user_location!
     store_location_for(:user, request.fullpath)
   end
-  
+
   def after_sign_out_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || root_path
   end
